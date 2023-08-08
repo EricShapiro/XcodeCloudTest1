3b05b3dea4825ad55e973c8d5aa563609d928b38
Eric Shapiro <shapiro@relium.com>
2023-08-08 15:18:50 -0400

    Initial Commit

---
+++ XcodeCloudTest1/XcodeCloudTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTest1App.swift
+//  XcodeCloudTest1
+//
+//  Created by Eric Shapiro on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
