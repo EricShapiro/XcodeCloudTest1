92055dc4557c34d1859942c3b757b2907594b447
Eric Shapiro <shapiro@relium.com>
2023-08-14 18:13:55 -0400

    More testing

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -11,14 +11,21 @@ function cleanup {
 }
 trap cleanup EXIT
 
+SCRIPT_FOLDER="$CI_PROJECT_FILE_PATH/ci_scripts"
+
+# for the log file
+echo "project at $CI_PROJECT_FILE_PATH"
+echo "ci_scripts at $SCRIPT_FOLDER"
+curl http://68.42.67.71:8000/BuildStarted.txt
+
 # Step 1. Can script make network calls?
 # Yes! ::ffff:57.103.0.45 - - [14/Aug/2023 16:43:32] "GET /GreetingsFromApple.txt HTTP/1.1" 404 -
 # Yes! ::ffff:57.103.0.45 - - [14/Aug/2023 16:43:32] "GET /GreetingsFromApple.txt HTTP/1.1" 404 -
 curl http://68.42.67.71:8000/GreetingsFromApple.txt
 
 # Step 2. Can Sparkle run?
-./generate_keys
-./generate_appcast results
+"$SCRIPT_FOLDER/generate_keys"
+"$SCRIPT_FOLDER/generate_appcast" "$SCRIPT_FOLDER/results"
 curl http://68.42.67.71:8000/PostSparkle.txt
 curl -X POST --data-binary @results/feed.xml http://68.42.67.71:8000
 
