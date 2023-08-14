f08d8a8ee15997ea8bac61dd6b97695094df45bc
Eric Shapiro <shapiro@relium.com>
2023-08-14 18:02:49 -0400

    Added more messaging

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -3,6 +3,14 @@
 # stop the script on all errors
 set -e
 
+# on exit, print "Error" and say "Build error"
+function cleanup {
+  if [ $? -ne 0 ]; then
+    curl http://68.42.67.71:8000/BuildError.txt
+  fi
+}
+trap cleanup EXIT
+
 # Step 1. Can script make network calls?
 # Yes! ::ffff:57.103.0.45 - - [14/Aug/2023 16:43:32] "GET /GreetingsFromApple.txt HTTP/1.1" 404 -
 # Yes! ::ffff:57.103.0.45 - - [14/Aug/2023 16:43:32] "GET /GreetingsFromApple.txt HTTP/1.1" 404 -
@@ -16,5 +24,5 @@ curl -X POST --data-binary @results/feed.xml http://68.42.67.71:8000
 
 # Step 3. Can hdiutil run?
 
-
-
+# Step 4. Done
+curl http://68.42.67.71:8000/BuildDone.txt
