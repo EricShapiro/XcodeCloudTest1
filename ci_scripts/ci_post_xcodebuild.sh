b3b4279f8f2d688632b659f1e36ce9554e44e49e
Eric Shapiro <shapiro@relium.com>
2023-08-14 17:18:37 -0400

    Xcode Cloud test #2: Run sparkle commands, generate feed.xml, and POST feed.xml

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+# stop the script on all errors
+set -e
+
 # Step 1. Can script make network calls?
+# Yes! ::ffff:57.103.0.45 - - [14/Aug/2023 16:43:32] "GET /GreetingsFromApple.txt HTTP/1.1" 404 -
+# Yes! ::ffff:57.103.0.45 - - [14/Aug/2023 16:43:32] "GET /GreetingsFromApple.txt HTTP/1.1" 404 -
 curl http://68.42.67.71:8000/GreetingsFromApple.txt
+
+# Step 2. Can Sparkle run?
+./generate_keys
+./generate_appcast results
+curl http://68.42.67.71:8000/PostSparkle.txt
+curl -X POST --data-binary @results/feed.xml http://68.42.67.71:8000
+
+# Step 3. Can hdiutil run?
+
 
 
